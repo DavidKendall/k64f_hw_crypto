1efdf1b0d0b40a29ec08624e09b63f1cac8434ba
David Kendall <david.kendall@northumbria.ac.uk>
2019-03-24 13:16:54 +0000

    Very basic timing of HW acceleration

--- main.cpp
+++ main.cpp
@@ -1,71 +1,102 @@
 #include <mbed.h>
-#define FREESCALE_MMCAU 1
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include "cau_api.h"
-#ifdef __cplusplus
-}
-#endif
+#include <stdint.h>
+#include "counter.h"
+
+enum controlData {N_ITERATIONS = 100};
 
 Serial pc(USBTX, USBRX, 115200);
 
-unsigned char md5_state[15] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 unsigned char key[16] = "0123456789abcde";
-unsigned char key_schedule[44 * 4];
 unsigned char plaintext[16] = "Confidential TX";
 unsigned char ciphertext[16] = "               ";
 unsigned char decryptedtext[16] = "               ";
 
 
 mbedtls_aes_context aes_ctx;
-unsigned char *key_sched2 = (unsigned char *)aes_ctx.buf;
+unsigned char *key_schedule = (unsigned char *)&aes_ctx.buf;
 
 void printBytesAsHex(const unsigned char *bytes, int len);
+void displayStats(uint32_t, uint32_t, uint32_t, uint64_t);
 
 int main() {
     int i;
     int ret;
+    uint32_t timeElapsed = 0;
+    uint32_t minTime = 0xFFFFFFFF;
+    uint32_t meanTime = 0;
+    uint32_t maxTime = 0;
+    uint64_t totalTime = 0;
 
-    pc.printf("MMCAU Test\n");
-    pc.printf("MD5 Init\n");
-    cau_md5_initialize_output(md5_state);
-    printBytesAsHex(md5_state, 15);
-    pc.printf("\n");
-    pc.printf("AES Set Key\n");
-    cau_aes_set_key(key, 128, key_schedule);
-    for (i = 0; i < 4; i += 1) {
-        printBytesAsHex(&key_schedule[i * 44], 44);
-        pc.printf("\n");
-    }
+    counterInit();
+    pc.printf("Time AES functions\n");
     pc.printf("MBED TLS Set Key\n");
     mbedtls_aes_init(&aes_ctx);
     ret = mbedtls_aes_setkey_enc(&aes_ctx, key, 128);
     for (i = 0; i < 4; i += 1) {
-        printBytesAsHex(&key_sched2[i * 44], 44);
+        printBytesAsHex(&key_schedule[i * 44], 44);
         pc.printf("\n");
     }
     pc.printf("AES Encrypt\n");
-    cau_aes_encrypt(plaintext, key_schedule, 10, ciphertext);
+    /* Reset all statistics
+     */
+    minTime = 0xFFFFFFFF;
+    meanTime = 0;
+    maxTime = 0;
+    totalTime = 0;
+    /* Repeatedly measure the execution time of the code in
+     * which we are interested - this is the 'Software under test'
+     */
+    for (int i = N_ITERATIONS; i > 0; i-=1) {
+      counterStart();
+      /********* Software under test *********************/
+      mbedtls_internal_aes_encrypt(&aes_ctx, plaintext, ciphertext);
+      /********* End Software under test ****************/
+      timeElapsed = counterStop();
+      if (timeElapsed < minTime) {
+        minTime = timeElapsed;
+      }
+      if (timeElapsed > maxTime) {
+        maxTime = timeElapsed;
+      }
+      totalTime += timeElapsed;
+    }
+
+    /* Display the statistics that have been captured */
+    meanTime = totalTime / N_ITERATIONS;
+    displayStats(minTime, meanTime, maxTime, totalTime);
     pc.printf("Plaintext : ");
     printBytesAsHex(plaintext, 16);
     pc.printf("\nCiphertext: ");
     printBytesAsHex(ciphertext, 16);
     pc.printf("\n");
     pc.printf("AES Decrypt\n");
-    cau_aes_decrypt(ciphertext, key_schedule, 10, decryptedtext);
-    pc.printf("Plaintext : ");
-    printBytesAsHex(decryptedtext, 16);
-    pc.printf("\n");
-    pc.printf("MBED TLS Encrypt\n");
-    mbedtls_internal_aes_encrypt(&aes_ctx, plaintext, ciphertext);
-    pc.printf("Plaintext : ");
-    printBytesAsHex(plaintext, 16);
-    pc.printf("\nCiphertext: ");
-    printBytesAsHex(ciphertext, 16);
-    pc.printf("\n");
-    pc.printf("MBED TLS Decrypt\n");
-    mbedtls_internal_aes_decrypt(&aes_ctx, ciphertext, decryptedtext);
+    /* Reset all statistics
+     */
+    minTime = 0xFFFFFFFF;
+    meanTime = 0;
+    maxTime = 0;
+    totalTime = 0;
+    /* Repeatedly measure the execution time of the code in
+     * which we are interested - this is the 'Software under test'
+     */
+    for (int i = N_ITERATIONS; i > 0; i-=1) {
+      counterStart();
+      /********* Software under test *********************/
+      mbedtls_internal_aes_decrypt(&aes_ctx, ciphertext, decryptedtext);
+      /********* End Software under test ****************/
+      timeElapsed = counterStop();
+      if (timeElapsed < minTime) {
+        minTime = timeElapsed;
+      }
+      if (timeElapsed > maxTime) {
+        maxTime = timeElapsed;
+      }
+      totalTime += timeElapsed;
+    }
+
+    /* Display the statistics that have been captured */
+    meanTime = totalTime / N_ITERATIONS;
+    displayStats(minTime, meanTime, maxTime, totalTime);
     pc.printf("Plaintext : ");
     printBytesAsHex(decryptedtext, 16);
     pc.printf("\n");
@@ -79,3 +110,14 @@ void printBytesAsHex(const unsigned char *bytes, int len) {
         pc.printf("%02x ", bytes[i]);
     }
 }
+
+void displayStats(uint32_t minTime,
+                  uint32_t meanTime,
+                  uint32_t maxTime,
+                  uint64_t totalTime) {
+  pc.printf("Min  : %020lu\n", minTime);
+  pc.printf("Mean : %020lu\n", meanTime);
+  pc.printf("Max  : %020lu\n", maxTime);
+  pc.printf("Tot  : %020llu\n", totalTime);
+}
+
